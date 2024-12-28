d3284c52bea04eec6baf8d7772b4f52d0e8bf354
nabeel-sadiq <nabeelsadiq.dev@gmail.com>
2024-12-28 18:02:55 +0500

    got chrome back

---
+++ chrome/date.mjs
@@ -0,0 +1,10 @@
+function getCurrentDate() {
+  const today = new Date();
+  const day = String(today.getDate()).padStart(2, "0");
+  const month = String(today.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+  const year = today.getFullYear();
+
+  return `${day}-${month}-${year}`;
+}
+
+export default getCurrentDate;
